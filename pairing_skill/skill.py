e9ca3c2ca264f5eedc90b044ed735c51f60ce94e
Matthew D. Scholefield <matthew3311999@gmail.com>
2017-09-18 12:02:11 -0500

    Fix license headers and unused import

--- pairing_skill/skill.py
+++ pairing_skill/skill.py
@@ -9,14 +9,14 @@
 # distributed with this work for additional information
 # regarding copyright ownership.  The ASF licenses this file
 # to you under the Apache License, Version 2.0 (the
-# 'License'); you may not use this file except in compliance
+# "License"); you may not use this file except in compliance
 # with the License.  You may obtain a copy of the License at
 #
 #   http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing,
 # software distributed under the License is distributed on an
-# 'AS IS' BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
@@ -30,7 +30,6 @@ from requests import HTTPError
 from mycroft import MycroftSkill
 from mycroft.api import DeviceApi, is_paired
 from mycroft.identity import IdentityManager
-from mycroft.util.log import LOG
 
 
 class PairingSkill(MycroftSkill):
