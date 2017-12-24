e3a95d00444fb981d7aaf6d3beffca8796a8891f
Matthew D. Scholefield <matthew3311999@gmail.com>
2017-12-23 18:37:56 -0600

    Add download and compile step to mimic

--- mycroft/frontends/tts/mimic_tts.py
+++ mycroft/frontends/tts/mimic_tts.py
@@ -1,8 +1,25 @@
 from subprocess import call
 
+from os.path import isdir
+
 from mycroft.frontends.tts.tts_plugin import TtsPlugin
+from mycroft.util.git_repo import GitRepo
 
 
 class MimicTts(TtsPlugin):
+    def __init__(self, rt):
+        super().__init__(rt)
+
+        if not isdir(self.rt.paths.mimic_exe):
+            self.download_mimic()
+
+    def download_mimic(self):
+        repo = GitRepo(self.rt.paths.mimic, self.config['url'], 'master')
+        repo.try_pull()
+        repo.run_inside('./dependencies.sh --prefix="/usr/local"')
+        repo.run_inside('./autogen.sh')
+        repo.run_inside('./configure.sh --prefix="/usr/local"')
+        repo.run_inside('make -j2')
+
     def read(self, text):
-        call(['mimic', '-t', text, '-voice', self.config['voice']])
+        call([self.rt.paths.mimic_exe, '-t', text, '-voice', self.config['voice']])
