ed8bb63043eb6444380956a75438cb9e2a8d5af1
Matthew D. Scholefield <matthew331199@gmail.com>
2019-08-15 13:27:55 -0500

    Make quit skill force quit after a second

--- quit_skill/skill.py
+++ quit_skill/skill.py
@@ -1,3 +1,8 @@
+from time import sleep
+
+import os
+from threading import Thread
+
 from mycroft_core import MycroftSkill, intent_handler
 
 
@@ -10,3 +15,8 @@ class QuitSkill(MycroftSkill):
     @intent_handler('quit')
     def hello(self):
         self.rt.main_thread.quit()
+        Thread(target=self.delay_force_quit, daemon=True).start()
+
+    def delay_force_quit(self):
+        sleep(1)
+        os._exit(0)
